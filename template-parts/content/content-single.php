3431f47ea7c52d1e8a927cf905c863a520399161
Pierre Saïkali <dev@mosaika.fr>
2018-10-25 15:47:15 +0200

    Removing the empty page title inside content elements.

---
+++ template-parts/content/content-single.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-content">
+		<?php
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentynineteen' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			)
+		);
+
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'twentynineteen' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php twentynineteen_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+
+	<?php get_template_part( 'template-parts/post/author', 'bio' ); ?>
+
+</article><!-- #post-${ID} -->
