b1ba7719dd9ec55efe7b1bb7e0db4417da5b8a60
Pierre Saïkali <dev@mosaika.fr>
2018-11-02 12:05:00 +0100

    Removing debugging data

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -12,8 +12,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<?php var_dump( $post->achievement->get_timeline() ); ?>
-
 	<div class="entry-content">
 		<?php
 		the_content(
